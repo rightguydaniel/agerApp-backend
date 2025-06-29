d84aaa5d3c6a0bb56620fa1247e9aa1ffab2165e
Rightguy <rightguydaniel@gmail.com>
2025-06-29 15:12:55 +0100

    fix

--- src/controllers/userControllers/signIn.ts
+++ src/controllers/userControllers/signIn.ts
@@ -9,9 +9,11 @@ export const signIn = async (request: Request, response: Response) => {
   try {
     if (!email) {
       sendResponse(response, 400, "Email is required to login");
+      return
     }
     if (!password) {
       sendResponse(response, 400, "Password is required to login");
+      return
     }
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
@@ -21,10 +23,12 @@ export const signIn = async (request: Request, response: Response) => {
     const user = await Users.findOne({ where: { email } });
     if (!user) {
       sendResponse(response, 400, `Account with ${email} dose not exist`);
+      return
     }
     const isPasswordValid = await verifyPassword(password, user?.password);
     if (!isPasswordValid) {
-      return sendResponse(response, 400, "Incorrect password");
+      sendResponse(response, 400, "Incorrect password");
+      return
     }
     const data = { id: user?.id, email: user?.email, role: user?.role };
 
@@ -33,6 +37,7 @@ export const signIn = async (request: Request, response: Response) => {
       user: { ...user?.get(), password: undefined },
       token,
     });
+    return
   } catch (error: any) {
     console.log(error.message);
     sendResponse(response, 500, "Internal Server Error", error.message);
