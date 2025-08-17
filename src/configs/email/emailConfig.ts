cbfdf1697869b8b8e53213622d3dd177ebcd28a3
Rightguy <rightguydaniel@gmail.com>
2025-08-17 19:22:11 +0100

    fix

--- src/configs/email/emailConfig.ts
+++ src/configs/email/emailConfig.ts
@@ -6,7 +6,6 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: `${process.env.MAIL_HOST}`,
   port: Number(process.env.MAIL_PORT),
-  secure: false,
   auth: {
     user: `${process.env.MAIL_USERNAME}`,
     pass: `${process.env.MAIL_PASSWORD}`,
